29c3c2171f916fc50a6bb3f856f0a66770f7abad
Radosław Waśko <wasko.radek@gmail.com>
2019-05-25 22:29:57 +0200

    Working on FSM, implemented some basic monads.

--- TFTP_Core.v
+++ TFTP_Core.v
@@ -4,6 +4,9 @@ Require Import Ascii.
 Require Import List.
 Require String.
 
+(* Require Import Binding. *)
+Set Implicit Arguments.
+
 Import ListNotations.
 
 Local Open Scope char_scope.
@@ -11,31 +14,206 @@ Local Open Scope char_scope.
 Axiom newline : ascii.
 Definition hello := ["h"; "i"].
 
-Inductive transfer : Set := upload : list ascii -> transfer | download : list ascii -> transfer.
-Inductive action : Set := send : list ascii -> Z -> action | terminate : action.
-Inductive state : Set := todo : state.
-Inductive input_event : Set := incoming : list ascii -> Z -> input_event | timeout : input_event.
+Inductive maybe (t : Set) := just : t -> maybe t | nothing : maybe t. 
 
-Definition incr1 (x : Z): Z :=
-  (x + 1).
+Definition message : Set := list ascii.
+Inductive transfer : Set := upload : message -> transfer | download : message -> transfer.
+Inductive action : Set := send : message -> N -> action | continue : action | terminate : action.
 
-Theorem incr1_lemma : forall x: Z, Z.lt x (incr1 x).
-intros.
-unfold incr1.
-omega.
-Qed.
+Inductive protocol_state : Set
+ := waiting_for_init_ack
+  | errored
+  | waiting_for_next_packet (sendertid : N) (blockid : nat)
+  | finished.
 
-Definition tst (x: ascii): ascii := x.
+Record state : Set := mkState
+ { fsm : protocol_state
+ ; previousMessage : maybe message
+ ; mytid : N
+ }.
+Inductive input_event : Set := incoming : message -> N -> input_event | timeout : input_event.
 
-Definition tst2 (x: list ascii): list ascii := x.
+(* Monadic code inspired by  file:///home/radeusgd/Projekty/TFTP/reference/MemMonad.html  TODO true link*)
+Definition stateM (ret : Set) := state -> state * ret.
 
-Definition make_upload (name: list ascii): transfer := upload name.
+Definition SReturn (T : Set) (v : T) : stateM T :=
+  fun m => (m, v).
 
-Definition initialize (tid : Z) (port : Z) (t : transfer) : (action * state) :=
-  (send hello port, todo).
+(*  operations on state *)
+Definition GetFSM : stateM protocol_state :=
+  fun m => (m, fsm m).
 
-Definition process_step (event : input_event) (st : state) : (action * state) :=
+Definition SetFSM (s : protocol_state) : stateM unit :=
+  fun m => (mkState s (previousMessage m) (mytid m), tt).
+
+Definition SBind (T1 T2 : Set) (M1 : stateM T1) (M2 : T1 -> stateM T2) : stateM T2 :=
+  fun m =>
+    let (m', v1) := M1 m in
+      M2 v1 m'.
+
+Class Monad (M : Set -> Set) := {
+  Return {T: Set} (v: T) : M T ;
+  Bind {T1 T2 : Set} (M1 : M T1) (M2 : T1 -> M T2) : M T2
+ }.
+
+Instance monad_stateM : Monad stateM :=
+ { Return := SReturn; Bind := SBind }.
+
+Notation "x <- m1 ; m2" := (Bind m1 (fun x => m2))
+  (right associativity, at level 60).
+Notation "m1 ;; m2" := (Bind m1 (fun _ => m2))
+  (right associativity, at level 60).
+
+Definition append (x : list ascii) (y : list ascii) : list ascii := concat [x; y].
+
+Notation "l1 <+> l2" := (append l1 l2) (right associativity, at level 30).
+(*
+Definition readMessageM (ret : Set) := message -> message * ret.
+
+Instance monad_readMessageM : Monad readMessageM :=
+  { Return T v := fun m => (m,v);
+    Bind T1 T2 M1 M2 := fun m =>
+    let (m', v1) := M1 m in
+      M2 v1 m' }.
+*)
+Section mspec.
+   Variables (T : Set) (P : state -> T -> Prop).
+
+   Definition mspec (m1 : stateM T) (m : state) : Prop :=
+    P (fst (m1 m)) (snd (m1 m)).
+
+   Lemma mspec_Return : forall m v,
+    P m v
+    -> mspec (Return v) m.
+    trivial.
+  Qed.
+End mspec.
+
+Section mspec_imp.
+   Variable T : Set.
+   Variables P1 P2 : state -> T -> Prop.
+  
+   Variable m1 : stateM T.
+   Variable m : state.
+
+  Theorem mspec_imp : mspec P1 m1 m
+    -> (forall m v, P1 m v -> P2 m v)
+    -> mspec P2 m1 m.
+    unfold mspec.
+    intuition.
+  Qed.
+End mspec_imp.
+
+(*Section mspec_Read.
+   Variable P : memory -> nat -> Prop.
+  
+   Variable addr : nat.
+   Variable m : memory.
+
+  Theorem mspec_Read :
+    P m (m addr)
+    -> mspec P (Read addr) m.
+    trivial.
+  Qed.
+End mspec_Read.
+*)
+(*
+Section mspec_Bind.
+   Variables (T1 T2 : Set) (P : state -> T2 -> Prop).
+
+   Variable m1 : stateM T1.
+   Variable m2 : T1 -> stateM T2.
+
+  Theorem mspec_Bind : forall m,
+    mspec (fun m v => mspec P (m2 v) m) m1 m
+    -> mspec P (Bind m1 m2) m.
+    unfold mspec, Bind, SBind; simpl; intro.
+    destruct (m1 m); simpl.
+    destruct (m2 t m). unfold SBind. intro. unfold stateM in m1, m2. auto. 
+    destruct (m1 m). destruct (m2 t1 s1). destruct (m2 t s) in H. intuition.
+
+  Qed.
+End mspec_Bind.
+*)
+
+Definition opRRQ := 1.
+Definition opDATA := 3.
+Definition opACK := 4.
+Definition opERROR := 5.
+
+Definition get_2b_int (msg : message) : maybe nat := match msg with
+  | a :: b :: _ => just ( (256 * (nat_of_ascii a) + (nat_of_ascii b)))
+  | _ => nothing nat
+  end.
+
+Definition nat_to_2b (n : nat) : message := [(ascii_of_nat (n / 256)); (ascii_of_nat n)].
+
+Definition get_message_id (msg : message) : maybe nat := get_2b_int msg.
+
+Fixpoint drop {T : Set} (n: nat) (l : list T) : list T := match (n, l) with
+  | (O, l) => l
+  | (_, []) => []
+  | (S k, _ :: t) => drop k t
+  end.
+
+Definition get_block_id (msg : message) : maybe nat := get_2b_int (drop 2 msg).
+
+Definition make_RRQ (filename: list ascii) : message :=
+  concat [[zero; ascii_of_nat opRRQ]; filename; [zero]; ["o"; "c"; "t"; "e"; "t"]; [zero]].
+
+Definition initialize (tid : N) (port : N) (f : list ascii): state * action :=
+  (mkState waiting_for_init_ack (nothing message) tid, send (make_RRQ f) port).
+
+Definition fail : stateM action := Return terminate. (* TODO send error *)
+
+Definition make_ACK (blockid : nat) : message :=
+  concat [nat_to_2b opACK; nat_to_2b blockid].
+
+Definition make_ERROR (errid : nat) : message :=
+  concat [nat_to_2b opERROR; nat_to_2b errid; [zero]].
+
+Definition get_data : message -> list ascii := drop 4.
+
+Definition data_length (msg : message) : nat := length (get_data msg).
+
+Definition handle_incoming_data (sender : N) (blockid : nat) (msg : message) : stateM action :=
+  SetFSM (waiting_for_next_packet sender (blockid + 1));;
+  (if data_length msg <? 512 then
+    (* this was the last block, finish up *)
+    SetFSM finished
+  else
+    Return tt);;
+  Return (send (make_ACK blockid) sender).
+
+Definition process_step (event : input_event) : stateM action :=
   match event with
-  | incoming msg sender => (send msg sender, st)
-  | timeout => (terminate, st)
+  | incoming msg sender => 
+    st <- GetFSM;
+    match st with
+    | waiting_for_init_ack => match get_message_id msg with
+      | just 3 => match get_block_id msg with
+          | just 1 => handle_incoming_data sender 1 msg
+          | _ => fail
+          end
+      | _ => fail
+      end
+    | errored => Return (terminate)
+    | waiting_for_next_packet sendertid expectedblockid => 
+       if N.eqb sender sendertid then
+         match get_message_id msg with
+         | just 3 => match get_block_id msg with
+            | just incomingblockid =>
+                if incomingblockid =? expectedblockid then
+                  handle_incoming_data sender incomingblockid msg
+                else if incomingblockid <? expectedblockid then
+                  Return continue (* probably earlier block has been retransmitted, so we ignore it *)
+                else fail
+            | _ => fail
+            end
+         | _ => fail
+         end
+       else Return (send (make_ERROR 5) sender)
+    | finished => Return (terminate)
+    end
+  | timeout => Return (terminate)
   end.
--- main.ml
+++ main.ml
@@ -21,7 +21,7 @@ let implode l =
 
 let str_to_list (s : string) : char list = explode s
 let list_to_str (l : char list) : string = implode l
-let fail message = Printf.printf "%s\n" message; exit 1
+let fail message = Printf.eprintf "%s\n" message; exit 1
 
 type transfer_direction = Upload of string | Download of string
 
@@ -30,11 +30,11 @@ let parse_args () =
   let port = ref 69 in
   let upload = ref "" in
   let download = ref "" in
-  Arg.parse [("-ip", Arg.Set_string ip, "IP address of server to connect to"); ("-port", Arg.Set_int port, "port of the server"); ("-download", Arg.Set_string download, "filename to download"); ("-upload", Arg.Set_string upload, "filename to upload")] (fun anon -> Printf.printf "Unrecognized argument: %s\n" anon; exit 1) "Simple TFTP client in OCaml and Coq";
+  Arg.parse [("-ip", Arg.Set_string ip, "IP address of server to connect to"); ("-port", Arg.Set_int port, "port of the server"); ("-download", Arg.Set_string download, "filename to download"); ("-upload", Arg.Set_string upload, "filename to upload")] (fun anon -> Printf.eprintf "Unrecognized argument: %s\n" anon; exit 1) "Simple TFTP client in OCaml and Coq";
   if !ip = "" then fail "IP not specified";
   if !upload = "" && !download = "" then fail "Provide a filename to download or upload";
   if !upload <> "" && !download <> "" then fail "You can either choose download or upload but not both";
-  if !upload != "" then (!ip, !port, Upload !upload)
+  if !upload <> "" then (!ip, !port, Upload !upload)
   else (!ip, !port, Download !download)
 
 let assign_random_TID () : int =
@@ -53,20 +53,23 @@ let create_udp_socket (tid : int) : file_descr =
 
 type state = TFTP_Core.state
 type message = bytes
-type action = Send of message * int | Terminate
+type action = Send of message * int | Terminate | Continue
 type event = Incoming of message * int | Timeout (* TODO should also provide IP? *) (*TODO add terminated event*)
 
-let translate_coq_result (coq_action, coq_state) : action * state =
+let translate_coq_result (coq_state, coq_action) : action * state =
   let action = match coq_action with
     | Coq_send (msg, port) -> Send (list_to_str msg, port)
-    | Coq_terminate -> Terminate in
+    | Coq_terminate -> Terminate
+    | Coq_continue -> Continue
+  in
   (action, coq_state)
 
 let initialize_connection (tid : int) (port : int) (transfer : transfer_direction) : action * state =
   let coq_transfer = match transfer with
-    | Upload fname -> TFTP_Core.Coq_upload (str_to_list fname)
-    | Download fname -> TFTP_Core.Coq_download (str_to_list fname)
+    | Upload fname -> fail "Upload is not implemented"
+    | Download fname -> str_to_list fname
   in
+  (* let init_state = TFTP_Core.initial_state in *)
   translate_coq_result (TFTP_Core.initialize tid port coq_transfer)
 
 let process_step (event :  event) (state : state) : action * state =
@@ -79,21 +82,15 @@ let max_packet_len = 600
 
 let main =
   Random.self_init(); (* initialize randomness *)
-  Printf.printf "hello world\n";
+  Printf.eprintf "hello world\n";
   let (ip, port, transfer) = parse_args() in
   let tid = assign_random_TID () in
-  Printf.printf "Connecting to %s:%d, my tid is %d\n%!" ip port tid;
+  Printf.eprintf "Connecting to %s:%d, my tid is %d\n%!" ip port tid;
   let fd = create_udp_socket tid in
   let make_addr port = ADDR_INET ((inet_addr_of_string ip), port) in
-  let rec loop (action, state) =
-    match action with
-    | Terminate -> Printf.printf "quitting\n"; exit 0
-    | Send (msg, port) ->
-      Printf.printf "sending '%s'\n%!" (Bytes.to_string msg);
-      let toaddr = make_addr port in
-      let sent = Unix.sendto fd msg 0 (Bytes.length msg) [] toaddr in
-      if sent <> Bytes.length msg then Printf.printf "Warning: message has not been sent whole, shouldn't ever happen with UDP! Sent %d/%d bytes" sent (Bytes.length msg);
-      Printf.printf "waiting for reply\n%!";
+  let rec
+    receive state =
+      Printf.eprintf "waiting for reply\n%!";
       let buf = Bytes.create max_packet_len in
       try
         let (recvd, sender) = Unix.recvfrom fd buf 0 max_packet_len [] in
@@ -101,13 +98,24 @@ let main =
         | ADDR_UNIX _ -> fail "Did not expect a UNIX message on UDP socket!"
         | ADDR_INET (fromip, fromport) ->
           if recvd <= 0 then (* TODO this is not a timeout but terminated conn *)
-            (Printf.printf "Receive error\n%!";
+            (Printf.eprintf "Receive error\n%!";
             loop (process_step Timeout state))
           else
-            (Printf.printf "received '%s'\n%!" (Bytes.to_string buf);
+            (Printf.eprintf "received '%s' from %d\n%!" (escaped (Bytes.to_string buf)) fromport;
             loop (process_step (Incoming (Bytes.sub buf 0 recvd, fromport)) state))
       with Unix.Unix_error (Unix.EAGAIN, "recvfrom", _) ->
-        Printf.printf "Timed out\n%!";
+        Printf.eprintf "Timed out\n%!";
         loop (process_step Timeout state)
+    and
+    loop (action, state) =
+    match action with
+    | Continue -> receive state
+    | Terminate -> Printf.eprintf "quitting\n"; exit 0
+    | Send (msg, port) ->
+      Printf.eprintf "sending '%s' to %d\n%!" (escaped (Bytes.to_string msg)) port;
+      let toaddr = make_addr port in
+      let sent = Unix.sendto fd msg 0 (Bytes.length msg) [] toaddr in
+      if sent <> Bytes.length msg then Printf.eprintf "Warning: message has not been sent whole, shouldn't ever happen with UDP! Sent %d/%d bytes" sent (Bytes.length msg);
+      receive state
   in
   loop (initialize_connection tid port transfer)
